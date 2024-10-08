22ee0036d5ceb87b6aa9765e24c7805a68247f26
Angus <angus.goody@gmail.com>
2024-10-08 11:45:01 +0100

    moved tests

--- app/Repositories/TimeRecordRepository.php
+++ app/Repositories/TimeRecordRepository.php
@@ -6,6 +6,10 @@ use App\Models\TimeRecord;
 use App\Contracts\TimeRecordRepositoryInterface;
 use Illuminate\Support\Collection;
 
+/**
+ * The TimeRecordRepository class is responsible for handling database operations
+ * related to time records. It is a concrete implementation of the TimeRecordRepositoryInterface.
+ */
 class TimeRecordRepository implements TimeRecordRepositoryInterface
 {
     public function createTimeRecord(array $data)
--- tests/Feature/Other/ExampleTest.php
+++ tests/Feature/Other/ExampleTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Feature;
+namespace Tests\Feature\Other;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
