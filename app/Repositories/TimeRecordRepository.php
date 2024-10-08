51db38c96c811cab5b68dd7c3e8c77b1d2911d2e
Angus <angus.goody@gmail.com>
2024-10-08 11:29:10 +0100

    Added TimeRecord repo and interface

---
+++ app/Repositories/TimeRecordRepository.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\TimeRecord;
+use App\Contracts\TimeRecordRepositoryInterface;
+
+class TimeRecordRepository implements TimeRecordRepositoryInterface
+{
+    public function createTimeRecord(array $data)
+    {
+        return TimeRecord::create($data);
+    }
+
+    public function getLastRecordForUser(int $userId)
+    {
+        return TimeRecord::where('user_id', $userId)->latest()->first();
+    }
+
+    public function getAllRecordsForUser(int $userId)
+    {
+        return TimeRecord::where('user_id', $userId)->get();
+    }
+}
+
