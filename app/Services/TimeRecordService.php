3d9bc9370a27a4bbf53abea916a2cceb8bd02df8
Angus <angus.goody@gmail.com>
2024-10-08 12:01:53 +0100

    added stubs for TimeRecordService

--- app/Services/TimeRecordService.php
+++ app/Services/TimeRecordService.php
@@ -18,13 +18,56 @@ class TimeRecordService
         $this->timeRecordRepository = $timeRecordRepository;
     }
 
+    /**
+     * Handle the clock in/out operation for the given user.
+     * @param int $userId
+     * @param string $userLocation
+     * @return void
+     */
+    public function handleClock(int $userId, string $userLocation)
+    {
+
+    }
+
+    /**
+     * Clock in the user.
+     * @param int $userId
+     * @return void
+     */
     public function clockIn(int $userId)
     {
         // Pass
     }
 
+    /**
+     * Clock out the user.
+     * @param int $userId
+     * @return void
+     */
     public function clockOut(int $userId)
     {
         // Pass
+    }
+
+    /**
+     * Check if the user clocks out too soon after clocking in. if so return true.
+     * @param Carbon $clockInTime
+     * @param Carbon $clockOutTime
+     * @return bool
+     */
+    protected function isSessionDurationTooShort(Carbon $clockInTime, Carbon $clockOutTime): bool
+    {
+        return $clockInTime->diffInSeconds($clockOutTime) < 10;
+    }
+
+    /**
+     * Convert the given time to UTC timezone.
+     * @param string $location
+     * @return Carbon
+     */
+    protected function convertToUTC(string $location): Carbon
+    {
+        $localTime = Carbon::now($location);
+        return $localTime->setTimezone('UTC');
     }
 }
---
+++ tests/Unit/Services/TimeRecordServiceTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Unit\Services;
+
+use Tests\TestCase;
+
+
+class TimeRecordServiceTest extends TestCase
+{
+    public function testClockIn()
+    {
+       // TODO
+    }
+}
