ade16b5902a7435004538e028a587502fdcdba42
Angus <angus.goody@gmail.com>
2024-10-10 20:26:13 +0100

    fixed test 1

--- tests/Unit/Repositories/LeaveRecordRepositoryTest.php
+++ tests/Unit/Repositories/LeaveRecordRepositoryTest.php
@@ -3,7 +3,7 @@
 namespace Tests\Unit\Repositories;
 
 use App\Contracts\LeaveRecordRepositoryInterface;
-use App\Enums\LeaveRecordType;
+use App\Models\LeaveType;
 use App\Models\User;
 use App\Repositories\LeaveRecordRepository;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -14,11 +14,15 @@ class LeaveRecordRepositoryTest extends TestCase
     use RefreshDatabase;
 
     protected LeaveRecordRepositoryInterface $leaveRecordRepository;
+    protected User $user;
+    protected LeaveType $leaveType;
 
     protected function setUp(): void
     {
         parent::setUp();
         $this->leaveRecordRepository = new LeaveRecordRepository;
+        $this->user = User::factory()->create();
+        $this->leaveType = LeaveType::factory()->create();
     }
 
     /**
@@ -28,12 +32,11 @@ class LeaveRecordRepositoryTest extends TestCase
      */
     public function testCreateLeaveRecord()
     {
-        $user = User::factory()->create();
         $data = [
-            'user_id' => $user->id,
+            'user_id' => $this->user->id,
             'start_date' => '2021-01-01 00:00:00',
             'end_date' => '2021-01-02 00:00:00',
-            'leave_type' => LeaveRecordType::ANNUAL,
+            'leave_type_id' => $this->leaveType->id,
         ];
 
         $leaveRecord = $this->leaveRecordRepository->createLeaveRecord($data);
@@ -42,7 +45,7 @@ class LeaveRecordRepositoryTest extends TestCase
         $this->assertEquals($data['user_id'], $leaveRecord->user_id);
         $this->assertEquals($data['start_date'], $leaveRecord->start_date);
         $this->assertEquals($data['end_date'], $leaveRecord->end_date);
-        $this->assertEquals($data['leave_type'], $leaveRecord->leave_type);
+        $this->assertEquals($data['leave_type_id'], $leaveRecord->leave_type_id);
     }
 
     /**
@@ -52,12 +55,11 @@ class LeaveRecordRepositoryTest extends TestCase
      */
     public function testDeleteLeaveRecord()
     {
-        $user = User::factory()->create();
         $data = [
-            'user_id' => $user->id,
+            'user_id' => $this->user->id,
             'start_date' => '2021-01-01 00:00:00',
             'end_date' => '2021-01-02 00:00:00',
-            'leave_type' => LeaveRecordType::ANNUAL,
+            'leave_type_id' => $this->leaveType->id,
         ];
 
         $leaveRecord = $this->leaveRecordRepository->createLeaveRecord($data);
@@ -76,26 +78,25 @@ class LeaveRecordRepositoryTest extends TestCase
      */
     public function testGetAllLeaveRecordsForUser()
     {
-        $user = User::factory()->create();
         $leaveRecordsData = [
-            ['user_id' => $user->id, 'start_date' => '2021-01-01 00:00:00', 'end_date' => '2021-01-02 00:00:00', 'leave_type' => LeaveRecordType::ANNUAL],
-            ['user_id' => $user->id, 'start_date' => '2021-01-03 00:00:00', 'end_date' => '2021-01-04 00:00:00', 'leave_type' => LeaveRecordType::SICK],
+            ['user_id' => $this->user->id, 'start_date' => '2021-01-01 00:00:00', 'end_date' => '2021-01-02 00:00:00', 'leave_type_id' => $this->leaveType->id],
+            ['user_id' => $this->user->id, 'start_date' => '2021-01-03 00:00:00', 'end_date' => '2021-01-04 00:00:00', 'leave_type_id' => $this->leaveType->id],
         ];
 
         foreach ($leaveRecordsData as $leaveRecordData) {
             $this->leaveRecordRepository->createLeaveRecord($leaveRecordData);
         }
 
-        $leaveRecords = $this->leaveRecordRepository->getAllLeaveRecordsForUser($user->id);
+        $leaveRecords = $this->leaveRecordRepository->getAllLeaveRecordsForUser($this->user->id);
 
         $this->assertCount(2, $leaveRecords);
         $this->assertEquals($leaveRecordsData[0]['user_id'], $leaveRecords[0]->user_id);
         $this->assertEquals($leaveRecordsData[0]['start_date'], $leaveRecords[0]->start_date);
         $this->assertEquals($leaveRecordsData[0]['end_date'], $leaveRecords[0]->end_date);
-        $this->assertEquals($leaveRecordsData[0]['leave_type'], $leaveRecords[0]->leave_type);
+        $this->assertEquals($leaveRecordsData[0]['leave_type_id'], $leaveRecords[0]->leave_type_id);
         $this->assertEquals($leaveRecordsData[1]['user_id'], $leaveRecords[1]->user_id);
         $this->assertEquals($leaveRecordsData[1]['start_date'], $leaveRecords[1]->start_date);
         $this->assertEquals($leaveRecordsData[1]['end_date'], $leaveRecords[1]->end_date);
-        $this->assertEquals($leaveRecordsData[1]['leave_type'], $leaveRecords[1]->leave_type);
+        $this->assertEquals($leaveRecordsData[1]['leave_type_id'], $leaveRecords[1]->leave_type_id);
     }
 }
