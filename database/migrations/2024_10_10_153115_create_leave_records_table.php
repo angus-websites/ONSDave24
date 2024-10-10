1a3a0a35abf1bd66a42ac41292eb09c9cf7c58cf
Angus <angus.goody@gmail.com>
2024-10-10 16:37:24 +0100

    added migration

--- database/migrations/2024_10_10_153115_create_leave_records_table.php
+++ database/migrations/2024_10_10_153115_create_leave_records_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\LeaveRecordType;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,15 @@ return new class extends Migration
     {
         Schema::create('leave_records', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')
+                ->references('id')
+                ->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->enum('type', LeaveRecordType::getValues())->default(LeaveRecordType::ANNUAL->value);
+            $table->dateTime('start');
+            $table->dateTime('end');
+            $table->text('reason')->nullable();
             $table->timestamps();
         });
     }
