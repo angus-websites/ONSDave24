52458172a03bb8db07d719dc196e17ff1a049fce
Angus <angus.goody@gmail.com>
2024-10-11 01:01:12 +0100

    another test

--- tests/Feature/Http/Controllers/TimeRecordControllerTest.php
+++ tests/Feature/Http/Controllers/TimeRecordControllerTest.php
@@ -41,4 +41,47 @@ class TimeRecordControllerTest extends TestCase
         ]);
 
     }
+
+    /**
+     * Test calling the clock endpoint twice
+     * as a user with a previous clock in record creates a clock
+     * in record and a clock out record
+     */
+    public function testHandleClockSecondTimeUser()
+    {
+        // Mock the current time
+        $now = Carbon::parse('2024-01-01 10:00:00');
+        Carbon::setTestNow($now);
+
+        // Create a user
+        $user = User::factory()->create();
+
+        // Create a clock in record for the user
+        $this->actingAs($user);
+        $this->get('/clock');
+
+        // Mock the current time
+        $now = Carbon::parse('2024-01-01 18:00:00');
+        Carbon::setTestNow($now);
+
+        // Call the clock endpoint
+        $this->get('/clock');
+
+        //Check two time records exist for the user
+        $this->assertDatabaseCount('time_records', 2);
+
+        // Check the content of the time records
+        $this->assertDatabaseHas('time_records', [
+            'user_id' => $user->id,
+            'recorded_at' => '2024-01-01 10:00:00',
+            'type' => 'clock_in',
+        ]);
+
+        $this->assertDatabaseHas('time_records', [
+            'user_id' => $user->id,
+            'recorded_at' => '2024-01-01 18:00:00',
+            'type' => 'clock_out',
+        ]);
+
+    }
 }
